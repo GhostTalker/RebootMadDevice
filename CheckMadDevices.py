8c464642b8376f2d128a711a07380a533e6d70c4
GhostTalker <pit@ghosttalker.de>
2020-05-11 21:13:39 +0200

    fix for mitm_auth

--- CheckMadDevices.py
+++ CheckMadDevices.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 __author__ = "GhostTalker"
 __copyright__ = "Copyright 2020, The GhostTalker project"
-__version__ = "1.2.21"
+__version__ = "1.2.22"
 __status__ = "Prod"
 
 # generic/built-in and other libs
@@ -119,12 +119,9 @@ class MonitoringItem(object):
     def check_status_page_headerauth(self, check_url, auth_token):
         """  Check Response Code and Output from status page """
         response = ""
-
         try:
-            session = requests.Session()
-            if self.mitm_pass:
-                session.headers = {'Content-Type': 'application/json', 'Authorization': auth_token}
-            response = session.get(check_url)
+            headers = { "Authorization": auth_token }
+            response = requests.get(url=check_url, headers=headers)
             response.raise_for_status()
             if response.status_code != 200:
                 logging.warning(
