6c372f9963921a2f5205434ac3874a8e0e79d045
GhostTalker <49254289+GhostTalker@users.noreply.github.com>
2020-07-06 22:32:29 +0200

    Bugfix: sleeptime after auth client

--- RMDclient.py
+++ RMDclient.py
@@ -4,7 +4,7 @@
 #
 __author__ = "GhostTalker"
 __copyright__ = "Copyright 2020, The GhostTalker project"
-__version__ = "2.0.6"
+__version__ = "2.0.7"
 __status__ = "PROD"
 
 # generic/built-in and other libs
